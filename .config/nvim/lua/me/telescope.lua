acbe88bb331bb04950b7e160203c6e98f9f14603
aoi-taniguchi <aoi.taniguchi@woven-planet.global>
2022-06-19 11:01:06 +0900

    feat: lazygit + telescope integration

--- .config/nvim/lua/me/telescope.lua
+++ .config/nvim/lua/me/telescope.lua
@@ -57,6 +57,7 @@ require('telescope').setup {
 
 require('telescope').load_extension('fzy_native')
 require('telescope').load_extension('file_browser')
+require('telescope').load_extension('lazygit')
 
 local M = {}
 M.search_dotfiles = function()
