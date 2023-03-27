2b90ba98f97661f300513f713d4af889657e0317
aoi-taniguchi <aoi.taniguchi@woven-planet.global>
2023-03-27 12:01:36 +0900

    feat: added aliases for dotfiles + lazygit

--- .bash_aliases
+++ .bash_aliases
@@ -9,7 +9,10 @@ alias ll=ee
 alias et='exa -T -L 3 -a -I "node_modules|.git|.cache" --icons'
 alias lt=et
 alias eta='exa -T -a -I "node_modules|.git|.cache" --color=always --icons | less -r'
+
 alias g='git'
+alias dg='git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 alias lg='lazygit'
+alias dlg='export GIT_DIR=$HOME/.dotfiles/ && export GIT_WORK_TREE=$HOME && lazygit && unset GIT_DIR && unset GIT_WORK_TREE'
+
 alias nv='~/appimages/nvim.appimage'
-alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -20,10 +20,13 @@ alias ll=ee
 alias et='exa -T -L 3 -a -I "node_modules|.git|.cache" --icons'
 alias lt=et
 alias eta='exa -T -a -I "node_modules|.git|.cache" --color=always --icons | less -r'
+
 alias g='git'
+alias dg='git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 alias lg='lazygit'
+alias dlg='set -x GIT_DIR $HOME/.dotfiles/ && set -x GIT_WORK_TREE $HOME && lazygit && set -e GIT_DIR && set -e GIT_WORK_TREE'
+
 alias nv='~/appimages/nvim.appimage'
-alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 
 starship init fish | source
 
