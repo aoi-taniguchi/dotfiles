3c5e0cdc6ae77d6dccf7f334a8d1c9483c45a896
aoi-taniguchi <aoi.taniguchi@woven-planet.global>
2023-11-23 00:33:47 +0900

    feat: fzf

--- .config/zsh/exports.zsh
+++ .config/zsh/exports.zsh
@@ -3,3 +3,13 @@
 HISTSIZE=1000000
 SAVEHIST=1000000
 
+export FZF_DEFAULT_OPTS="
+    --height 90% --reverse --border
+    --prompt='➜  ' --margin=0,1 --inline-info
+    --tiebreak=index --filepath-word
+"
+
+export FZF_CTRL_T_COMMAND="fd --type f "
+export FZF_CTRL_T_OPTS="
+    --select-1 --exit-0
+    --preview 'bat -r :100 --color=always --style=header,grid,numbers {}'"
