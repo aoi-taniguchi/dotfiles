4e7a13cb6536182b1905662f897bdb8fb401fab5
aoi-taniguchi <aoi.taniguchi@woven-planet.global>
2023-12-09 12:22:29 +0900

    feat: updated the directory structure for stow

--- zsh/.config/zsh/aliases.zsh
+++ zsh/.config/zsh/aliases.zsh
@@ -10,6 +10,7 @@ alias ll=ee
 alias et='exa -T -L 3 -a -I "node_modules|.git|.cache" --icons'
 alias lt=et
 alias eta='exa -T -a -I "node_modules|.git|.cache" --color=always --icons | less -r'
+alias lta='exa -T -a -I "node_modules|.git|.cache" --color=always --icons | less -r'
 
 alias g='git'
 alias dg='git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
